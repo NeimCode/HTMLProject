34ae795b995f276568524ce91e5aa50544f59a77
NeimCode <CutyShyHealt@gmail.com>
2025-05-12 11:27:37 +0200

    `Refactor navigation menu and add dynamic time display`

--- Trabajo HTML/js/script.js
+++ Trabajo HTML/js/script.js
@@ -1,21 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     function tiempo() {
-        let updateTime = () => {
-            let date = new Date();
-            let formattedDate = date.toLocaleString('ES-ES', {
-                weekday: 'long',
-                year: 'numeric',
-                month: 'long',
-                day: 'numeric',
-                hour: '2-digit',
-                minute: '2-digit',
-                second: '2-digit'
-            });
-            document.getElementById("tiempo").innerHTML = formattedDate;
-        };
-
-        setInterval(updateTime, 1000);
+        setInterval(function() {
+            document.getElementById("tiempo").innerHTML = new Date().toLocaleString();
+        }, 1000);
     }
 
     tiempo();
