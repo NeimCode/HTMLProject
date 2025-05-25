bda5178c4b259476ee023a0efb9d28f22199f9d5
NeimCode <CutyShyHealt@gmail.com>
2025-05-25 11:02:21 +0200

    `Added tooltip functionality to menu items, implemented dropdown menu, and modified JavaScript and CSS files to support these changes.`

--- Trabajo HTML/js/script.js
+++ Trabajo HTML/js/script.js
@@ -1,12 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    const style = window.getComputedStyle(document.body);
+    const colorActual = style.getPropertyValue('background-color');
+
+        function alerta(color) {
+        alert(`Hola, has entrat a la página. El color actual es ${color}`);
+    }
+    
+    alerta(colorActual);
+
     function tiempo() {
         setInterval(function() {
             document.getElementById("tiempo").innerHTML = new Date().toLocaleString();
         }, 1000);
     }
+    
+document.addEventListener("click", (event) => {
+        const randomColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+        document.body.style.backgroundColor = randomColor;
+});
+
 
     tiempo();
+
+    const dropdownButton = document.getElementById("dropdown-button");
+    const dropdownMenu = document.getElementById("dropdown-menu");
+
+    dropdownButton.addEventListener("click", function() {
+      if (dropdownMenu.style.display === "block") {
+        dropdownMenu.style.display = "none";
+      } else {
+        dropdownMenu.style.display = "block";
+      }
+    });
+
+    
+
+
 
 })
 
